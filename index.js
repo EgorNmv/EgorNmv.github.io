3ce082284fd4a56c6076fd410307678cb118764b
Egor Naumov <egor_nmv@bk.ru>
2023-07-23 21:52:51 +0500

    removed files

--- index.js
+++
@@ -1,9 +0,0 @@
-// // async function main() {
-// //   const response = await fetch("https://ultramining.com/crypto-calc/")
-// //     .then((r) => r.text())
-// //     .then((data) => data);
-
-// //   console.info(response);
-// // }
-
-// main();
