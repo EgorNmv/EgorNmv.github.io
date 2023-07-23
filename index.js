198d3d1979d0286faec29d99c040f4027dbe1568
Egor Naumov <egor_nmv@bk.ru>
2023-07-23 21:06:22 +0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// // async function main() {
+// //   const response = await fetch("https://ultramining.com/crypto-calc/")
+// //     .then((r) => r.text())
+// //     .then((data) => data);
+
+// //   console.info(response);
+// // }
+
+// main();
